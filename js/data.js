7b0715448b12412e40d68764579e2dcb60bc1c4d
Greg Doud <gpdoud@gmail.com>
2019-10-04 16:01:53 -0400

    initial commit

---
+++ js/data.js
@@ -0,0 +1,36 @@
+
+var users = [
+    {
+        "id": 1,
+        "username": "adm",
+        "password": "adm",
+        "firstname": "Normal",
+        "lastname": "Admin",
+        "phone": "513-555-1212",
+        "email": "help@admin.com",
+        "isReviewer": true,
+        "isAdmin": true
+    },
+    {
+        "id": 2,
+        "username": "user",
+        "password": "user",
+        "firstname": "Normal",
+        "lastname": "User",
+        "phone": "",
+        "email": "",
+        "isReviewer": false,
+        "isAdmin": false
+    },
+    {
+        "id": 3,
+        "username": "sa",
+        "password": "sa",
+        "firstname": "System",
+        "lastname": "Admin",
+        "phone": "",
+        "email": "",
+        "isReviewer": true,
+        "isAdmin": true
+    }
+]
