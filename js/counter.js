9ea147ddbc2682be3a8ca3fecda73fd47662067c
Greg Doud <gpdoud@gmail.com>
2019-10-07 16:33:23 -0400

    Added bowling and counter

--- js/counter.js
+++ js/counter.js
@@ -7,6 +7,11 @@ var display = () => {
         nbrCtrl.style.color = (nbr % 2 == 0) ? "red" : "black";
         nbrCtrl.style.fontWeight = (nbr % 3 == 0) ? "bold" : "normal";
         nbrCtrl.style.fontStyle = (nbr % 5 == 0) ? "italic" : "normal";
+        // if(nbr % 5 == 0) {
+        //     nbrCtrl.style.fontStyle = "italic";
+        // } else {
+        //     nbrCtrl.style.fontStyle = "normal";
+        // }
     }
 }
 var increment = () => {
