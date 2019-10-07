c84a64ccf8fdc9560f807e7733e156655e7fc86e
Greg Doud <gpdoud@gmail.com>
2019-10-07 11:21:02 -0400

    Added multiple html files
    1) userdetail.html
    2) counter.html

---
+++ js/counter.js
@@ -0,0 +1,22 @@
+let nbr = 0;
+
+var display = () => {
+    var nbrCtrl = document.getElementById("nbr");
+    nbrCtrl.value = nbr;
+    {   // code to implement stage 2 requirements
+        // nbrCtrl.style.color = (nbr % 2 == 0) ? "red" : "black";
+        // nbrCtrl.style.fontWeight = (nbr % 3 == 0) ? "bold" : "normal";
+        // nbrCtrl.style.fontStyle = (nbr % 5 == 0) ? "italic" : "normal";
+    }
+}
+var increment = () => {
+    nbr++;
+    display();
+}
+var decrement = () => {
+    nbr--;
+    display();
+}
+var loaded = () => {
+    display();
+}
